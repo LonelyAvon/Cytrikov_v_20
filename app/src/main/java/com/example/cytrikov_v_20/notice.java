4d68fd4049434e506356fc3efdfacc50f8ddf55e
lonelymaple <cytrikov.m@mail.ru>
2024-01-17 19:29:39 +0700

    second commit

---
+++ app/src/main/java/com/example/cytrikov_v_20/notice.java
@@ -0,0 +1,14 @@
+package com.example.cytrikov_v_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class notice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+    }
+}
