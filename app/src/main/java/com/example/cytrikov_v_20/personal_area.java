7c6bdc5d5246454de9a088ff762ef1fd5803a6c1
lonelymaple <cytrikov.m@mail.ru>
2024-01-17 18:59:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cytrikov_v_20/personal_area.java
@@ -0,0 +1,14 @@
+package com.example.cytrikov_v_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class personal_area extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_area);
+    }
+}
